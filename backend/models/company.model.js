95dbe1057f2340a534280584b48dbadd793b967e
kothapallivinaykumar <165812777+vinay2902H@users.noreply.github.com>
2025-07-14 20:02:15 +0530

    Update company.model.js

--- backend/models/company.model.js
+++ backend/models/company.model.js
@@ -16,7 +16,7 @@ const companySchema = new mongoose.Schema({
         type:String 
     },
     logo:{
-        type:String // URL to company logo
+        type:String 
     },
     userId:{
         type:mongoose.Schema.Types.ObjectId,
