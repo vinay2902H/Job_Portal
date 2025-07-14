25928ab11d5292f8f4e245873a24dbdb3b79a8b0
kothapallivinaykumar <165812777+vinay2902H@users.noreply.github.com>
2025-07-14 20:01:55 +0530

    Update application.model.js

--- backend/models/application.model.js
+++ backend/models/application.model.js
@@ -3,20 +3,20 @@ import mongoose from 'mongoose';
 const applicationSchema = new mongoose.Schema({
   job: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Job', // Reference to the Job model
+    ref: 'Job', 
     required: true,
   },
   applicant: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User', // Reference to the User (applicant) model
+    ref: 'User',
     required: true,
   },
   status: {
     type: String,
-    enum: ['pending', 'accepted', 'rejected'], // Possible values for the application status
-    default: 'pending', // Default status when an application is created
+    enum: ['pending', 'accepted', 'rejected'], 
+    default: 'pending', 
   }
-}, { timestamps: true }); // Automatically adds createdAt and updatedAt fields
+}, { timestamps: true }); 
 
-// Create the Application model
+
 export const Application = mongoose.model("Application", applicationSchema);
